7c79311df94932a216c225d8c5fea0ad564d0066
ross-brown <rbrown_98@yahoo.com>
2024-02-28 21:03:40 -0600

    initial commit with featured products

---
+++ components/footer.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          &copy; {new Date().getFullYear()} A&B Liquor, LLC.
+        </p>
+        <p className="text-center text-xs text-black">
+          Created by <a href="http://linkedin.com/in/ross-brown-" target="_blank">
+            Ross Brown
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
